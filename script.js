db1352d64cee19f3d98170920c857967f6b93679
Antonio <84136604+JinZayex@users.noreply.github.com>
2024-11-07 17:42:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 var youtubePlayer;
 var scWidget;
-var mediaItems = [
-  { type: 'youtube', id: 'tgbNymZ7vqY' },   // YouTube video
-  { type: 'youtube', id: 'hipBlnFz95M' },   // YouTube video
-  { type: 'soundcloud', id: '858345298' },  // SoundCloud track
-  { type: 'soundcloud', id: '858345292' },  // SoundCloud track
-  { type: 'youtube', id: 'lTTajzrSkCw' }    // YouTube video
+
+var BASE64Q = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-"
+
+var ARTISTS = ["None","Militant P", "Papa Buju"];
+
+var SONG_NAME = [
+  "Struggle to live",
+  "BAM BAM",
+  "No CBD",
+  "Tre Cassett",
+  "Pisello outta R'N'S crew & Papa Buju live @ Uno GT Radio Show"
+];
+
+var MEDIAITEMS = [
+  { idItem: '0', type: 'youtube', link: 'LoEKXyGd2aE', artists: '001001001' },
+  { idItem: '1', type: 'youtube', link: 'Gc0HIOOnnFg', artists: '002002002' },
+  { idItem: '2', type: 'youtube', link: 'hipBlnFz95M', artists: '002002002' },
+  { idItem: '3', type: 'youtube', link: '9LycOp1JGvk', artists: '002002002' },
+  { idItem: '4', type: 'youtube', link: 'qXpLITKdd44', artists: '002002002' }
 ];
 
 var currentMediaIndex = -1;
 var progressBar = document.getElementById('progressBar');
 var progressKnob = document.getElementById('progressKnob');
 var progressContainer = document.querySelector('.progress-container');
+
+
+
+function fromBase64QtoInt(stringa){
+    var integer = 0;
+    for (let i = stringa.length-1; i > 0 ; i--) {
+      var a = stringa[i];
+      var t = BASE64Q.indexOf(stringa[i]);
+      var b = (64**(stringa.length-i-1));
+      integer += BASE64Q.indexOf(stringa[i]) * (64**(stringa.length-i-1));
+    }
+    return integer;
+}
 
 function onYouTubeIframeAPIReady() {
   youtubePlayer = new YT.Player('youtubePlayer', {
@@ -39,29 +65,45 @@ function stopAllPlayers() {
   }
 }
 
+
 function loadMedia(index) {
-  if (index >= 0 && index < mediaItems.length) {
+  if (index >= 0 && index < MEDIAITEMS.length) {
     currentMediaIndex = index;
-    var nextMedia = mediaItems[currentMediaIndex];
+    var nextMedia = MEDIAITEMS[currentMediaIndex];
     stopAllPlayers(); // Stop any currently playing media
 
     if (nextMedia.type === 'youtube') {
-      youtubePlayer.loadVideoById(nextMedia.id);
+      youtubePlayer.loadVideoById(nextMedia.link);
     } else if (nextMedia.type === 'soundcloud') {
-      scWidget.load('https://api.soundcloud.com/tracks/' + nextMedia.id, {
-        auto_play: true,
-        show_comments: true,
-        show_user: true,
-        show_reposts: false,
-        visual: true
-      });
+      //scWidget.load('https://api.soundcloud.com/tracks/' + nextMedia.id, {
+      //  auto_play: true,
+      //  show_comments: true,
+      //  show_user: true,
+      //  show_reposts: false,
+      //  visual: true
+      // });
     }
     document.getElementById('message').innerHTML = 'Canzone corrente: ' + currentMediaIndex;
+    document.getElementById('songTitle').innerHTML = SONG_NAME[currentMediaIndex];
+
+    var artisti = "";
+    for (let i = 0; i < 3; i++){
+        let artistId = fromBase64QtoInt(MEDIAITEMS[currentMediaIndex]);
+        if (artistId !== 0){
+          artisti += MEDIAITEMS[artistId] + ", ";
+        }
+    }
+
+    document.getElementById('songArtists').innerHTML = artisti;
+
+
   }
 }
 
 function loadNextMedia() {
   loadMedia(currentMediaIndex + 1);
+  var playButton = document.querySelector(".button-control:nth-child(2)");
+  playButton.innerHTML = '&#9209';
 }
 
 function loadPreviousMedia() {
@@ -69,27 +111,41 @@ function loadPreviousMedia() {
 }
 
 function playCurrentMedia() {
-    if (currentMediaIndex >= 0 && currentMediaIndex < mediaItems.length) {
-        var currentMedia = mediaItems[currentMediaIndex];
-        
-        if (currentMedia.type === 'youtube') {
-            var playerState = youtubePlayer.getPlayerState();
-            if (playerState === YT.PlayerState.PLAYING) {
-                youtubePlayer.pauseVideo(); // Metti in pausa
-            } else if (playerState === YT.PlayerState.PAUSED || playerState === YT.PlayerState.CUED) {
-                youtubePlayer.playVideo(); // Riprendi la riproduzione
-            }
-        } else if (currentMedia.type === 'soundcloud') {
-            scWidget.isPaused(function(isPaused) {
-                if (isPaused) {
-                    scWidget.play(); // Riprendi la riproduzione
-                } else {
-                    scWidget.pause(); // Metti in pausa
-                }
-            });
-        }
+  var playButton = document.querySelector(".button-control:nth-child(2)");
+
+  if (currentMediaIndex >= 0 && currentMediaIndex < MEDIAITEMS.length) {
+    var currentMedia = MEDIAITEMS[currentMediaIndex];
+
+    if (currentMedia.type === 'youtube') {
+      var playerState = youtubePlayer.getPlayerState();
+      if (playerState === YT.PlayerState.PLAYING) {
+        youtubePlayer.pauseVideo();
+        playButton.innerHTML = '&#9205';
+      } else if (playerState === YT.PlayerState.PAUSED || playerState === YT.PlayerState.CUED) {
+        youtubePlayer.playVideo();
+        playButton.innerHTML = '&#9209';
+      }
+    } else if (currentMedia.type === 'soundcloud') {
+      //scWidget.isPaused(function(isPaused) {
+       // if (isPaused) {
+        //  scWidget.play();
+        //  playButton.innerHTML = '&#9209';
+        //} else {
+       //   scWidget.pause();
+       //   playButton.innerHTML = '&#9209';
+       // }
+     // });
     }
   }
+}
+
+function nextMedia() {
+  loadNextMedia();
+}
+
+function previousMedia() {
+  loadPreviousMedia();
+}
 
 function formatTime(seconds) {
   var minutes = Math.floor(seconds / 60);
@@ -108,41 +164,14 @@ function updateProgressBar() {
     progressKnob.style.left = progress + '%';
     timeDisplay.innerHTML = formatTime(currentTime) + " / " + formatTime(duration);
   }
-
-  if (scWidget) {
-    // scWidget.getCurrentPosition(function(position) {
-    // scWidget.getDuration(function(duration) {
-    // var progress = (position / duration) * 100;
-    // progressBar.style.width = progress + '%';
-    // progressKnob.style.left = progress + '%';
-    // timeDisplay.innerHTML = formatTime(position) + " / " + formatTime(duration);
-    // });
-    // });
-  }
 }
 
-// Inizializza il player di SoundCloud
-var iframeElement = document.getElementById('soundcloudPlayer');
-scWidget = SC.Widget(iframeElement);
-
-scWidget.bind(SC.Widget.Events.READY, function() {
-  scWidget.bind(SC.Widget.Events.PLAY, function() {
-    updateProgressBar();
-  });
-  scWidget.bind(SC.Widget.Events.FINISH, function() {
-    loadNextMedia();
-  });
-});
-
-// Carica il primo media
 window.onload = function() {
   loadNextMedia();
 };
 
-// Aggiorna la barra di avanzamento ogni secondo
 setInterval(updateProgressBar, 1000);
 
-// Gestione del drag-and-drop del cursore di avanzamento
 let isDragging = false;
 
 progressKnob.addEventListener('mousedown', function(e) {
@@ -175,4 +204,18 @@ function onMouseUp() {
   isDragging = false;
   document.removeEventListener('mousemove', onMouseMove);
   document.removeEventListener('mouseup', onMouseUp);
+}
+
+
+function toggleMediaVisibility() {
+  const youtubeIframe = document.getElementById('youtubePlayer');
+
+  // Toggle display for YouTube iframe
+  if (youtubeIframe.style.display === 'none' || youtubeIframe.style.display === '') {
+    youtubeIframe.style.display = 'block';
+  } else {
+    youtubeIframe.style.display = 'none';
+  }
+
+
 }
